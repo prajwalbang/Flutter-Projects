d0ed367a83315358b75bbf445733db1e35a06727
Cyber-Prajwal <76061831+Cyber-Prajwal@users.noreply.github.com>
2022-02-26 17:32:44 +0530

    Add files via upload
    In this folder there is the subfolder lib which contains the dart files, well about this app it displays some famous quotes in padded containers with each quote having a delete option to delete any specific quote and if app is restarted data returns , soon will try to include ''add quote button'' as well with permanent data change .

---
+++ quotes/lib/quote_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  final Quote quote;
+  final VoidCallback delete;
+
+  const QuoteCard({ required this.quote, required this.delete });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextButton.icon(
+                onPressed:delete,
+                icon: Icon(Icons.delete),
+                label: Text('Delete Quote'))
+          ],
+        ),
+      ),
+    );
+  }
+}
