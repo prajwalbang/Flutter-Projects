09b227a92fcca1181d39f18a216483fa549c1a01
Cyber-Prajwal <76061831+Cyber-Prajwal@users.noreply.github.com>
2023-04-27 00:43:38 +0530

    Add files via upload

---
+++ ghar_ka_khana_2/lib/screens/loginScreen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:ghar_ka_khana_2/screens/forgetPwScreen.dart';
+import '../const/colors.dart';
+import '../utils/helper.dart';
+import '../screens/signUpScreen.dart';
+import '../widgets/customTextInput.dart';
+import './introScreen.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = "/loginScreen";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: Helper.getScreenHeight(context),
+        width: Helper.getScreenWidth(context),
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 30,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "Login",
+                  style: Helper.getTheme(context).headline6,
+                ),
+                Spacer(),
+                Text("Add your details to login"),
+                Spacer(),
+                CustomTextInput(
+                  hintText: "Your E-Mail ID",
+                ),
+                Spacer(),
+                CustomTextInput(
+                  hintText: "Password",
+                ),
+                Spacer(),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushReplacementNamed(IntroScreen.routeName);
+                    },
+                    child: Text("Login"),
+                  ),
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(ForgetPwScreen.routeName);
+                  },
+                  child: Text("Forgot Your Password?"),
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+                Text("Or Login with "),
+                Spacer(),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Color(
+                          0xFF367FC0,
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          Helper.getAssetName(
+                            "fb.png",
+                            "virtual",
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text("Login with Facebook")
+                      ],
+                    ),
+                  ),
+                ),
+                Spacer(),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Color(
+                          0xFFDD4B39,
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          Helper.getAssetName(
+                            "google.png",
+                            "virtual",
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text("Login with Google")
+                      ],
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 4,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(SignUpScreen.routeName);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an Account? "),
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: AppColor.orange,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
